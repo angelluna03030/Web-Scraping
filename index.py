392ff4ee6cea1052288b53bb0c8bf57c9f83ee42
angel luna <angelstivengarcialuna@gmail.com>
2025-05-14 19:31:31 -0500

    logica de progracion

--- index.py
+++ index.py
@@ -1,2 +1,10 @@
 print("Hello World")
 
+x = 3.17
+def my_function(arg1, arg2):
+    if arg1 > arg2:
+        print("arg1 is greater than arg2")
+    else:
+        print("arg1 is not greater than arg2")
+
+my_function(5, 2)
